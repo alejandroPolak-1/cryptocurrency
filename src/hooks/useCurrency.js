14bba45d9f9d0fbb6cbf1ec472f15f4b2299961e
alejandroPolak <alejandro_polak@hotmail.com>
2020-05-07 13:42:15 -0300

    [Read user select of custom hook] [to correct] option.cod -> option.code

--- src/hooks/useCurrency.js
+++ src/hooks/useCurrency.js
@@ -7,11 +7,13 @@ const useCurrency = (label, stateInitial, options) => {
   const Select = () => (
     <Fragment>
       <label>{label}</label>
-      <select>
+      <select
+      onChange={ e=> setState(e.target.value)}
+      >
         <option value="">- Select -</option>
         
         {options.map((option) => (
-          <option key={option.cod} value={option.cod}>
+          <option key={option.code} value={option.code}>
             {option.name}
           </option>
         ))}
