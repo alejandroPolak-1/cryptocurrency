f4faa9d740b54d9332b69d798b4b9af7003477b6
alejandroPolak <alejandro_polak@hotmail.com>
2020-05-07 11:32:00 -0300

    [Styled component]

--- src/App.js
+++ src/App.js
@@ -1,8 +1,56 @@
 import React from 'react';
+import styled from '@emotion/styled'
+import image from './cryptologo.png'
+
+const Conteiner = styled.div`
+max-width:1020px;
+margin: 0 auto;
+@media (min-width:1022px) {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  column-gap: 2rem;
+}
+`
+
+const Image = styled.img`
+max-width: 100%;
+margin-top: 5rem;
+
+`
+
+const Heading= styled.h1`
+font-family: 'Bebas Neue', cursive;
+color: #fff;
+text-align:left;
+font-weight: 700;
+font-size:50px;
+margin-bottom: 50px;
+margin-top: 50px;
+
+&::after{
+  content:'';
+  width: 100px;
+  height: 6px;
+  background-color: #66A2FE;
+  display:block;
+}
+`
+
+
 
 function App() {
   return (
-   <h1> Crypto currency</h1>
+   <Conteiner>
+     <div>
+     <Image
+      src={image}
+      alt="cryptocurrencies image"
+     />
+     </div>
+     <div>
+<Heading> Cryptocurrencies Quotes </Heading>
+     </div>
+   </Conteiner>
   );
 }
 
