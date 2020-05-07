0fe9d15d8e160e8e9857ab1532dbb1faa3037c2c
alejandroPolak <alejandro_polak@hotmail.com>
2020-05-07 14:25:43 -0300

    [styled Component currect hook]

--- src/hooks/useCurrency.js
+++ src/hooks/useCurrency.js
@@ -1,13 +1,36 @@
 import React, { Fragment, useState } from 'react'
+import styled from '@emotion/styled'
 
+//styled component
+const Label = styled.label`
+font-family: 'Bebas Neue', cursive;
+color: #FFF;
+text-transform: uppercase;
+font-weight: bold;
+font-size: 2.4rem;
+margin-top: 2rem;
+display: block;
+`
+const Select = styled.select`
+width: 100%;
+display: block;
+padding: 1rem;
+-webkit-appearance: none;
+border-radius: 10px;
+border:none;
+font-size: 1.2rem;
+text-align-last: center;
+`
+
+//custom hook
 const useCurrency = (label, stateInitial, options) => {
   //State of own hooks
   const [state, setState] = useState(stateInitial)
 
-  const Select = () => (
+  const ToSelect = () => (
     <Fragment>
-      <label>{label}</label>
-      <select
+      <Label>{label}</Label>
+      <Select
       onChange={ e=> setState(e.target.value)}
       >
         <option value="">- Select -</option>
@@ -17,12 +40,12 @@ const useCurrency = (label, stateInitial, options) => {
             {option.name}
           </option>
         ))}
-      </select>
+      </Select>
     </Fragment>
   )
 
   // Retur state, interface and function that modifies the state
-  return [state, Select, setState]
+  return [state, ToSelect, setState]
 }
 
 export default useCurrency
